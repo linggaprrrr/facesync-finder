1a723e8c96c6f805fa859c43c6a1c796f1337e87
Lingga Pangestu <pangestur11@gmail.com>
2025-07-14 19:16:05 +0700

    add logo

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import sys
+import os
+from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import (
     QApplication, QMessageBox, QDialog
 )
-
 from ui.admin_setup_dialogs import AdminSetupDialog
 from ui.admin_login import AdminLoginDialog
 from config.config_manager import ConfigManager
@@ -13,6 +14,9 @@ class MainApplication:
     
     def __init__(self):
         self.app = QApplication(sys.argv)
+         # Set logo aplikasi
+        logo_path = os.path.join(os.path.dirname(__file__), "assets", "ownize_logo.png")
+        self.app.setWindowIcon(QIcon(logo_path))
         self.config_manager = ConfigManager()
         self.main_window = None
     
